2b7ee6ac96f79edfab786e9f5541f932f8c0c8e8
lillib123 <lblair2@emich.edu>
2017-11-07 13:24:45 -0500

    fixed mobile resposiveness, finished part1 requirements

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
 $(document).ready(function() {
 //--------------------------------
-
+var container;
+var users_name;
+var users_number;
+var party_size;
+var inside_text;
 
 $("#form").hide();
-
 $(".circles").addClass("available");
-
-$("#close").click(function() {
+//---------------------------------------
+$("#close").on("click", function() {
   $("#form").fadeOut(300);
 });
-
-$(".circles").click(function(){
+//--------------------------------------
+$(".circles").on("click", function(event){
+  container = $(this);
+  inside_text = (container["0"].innerHTML);
   $("#form").fadeIn(300);
-  console.log($(this).attr("class"));
-
-    if ($(this).attr("class") === "circles available reserved") {
-      $(this).css("cursor", "notAvailable");
-      $("#form").hide();
-    } else
-      $(this).addClass("reserved");
+  $("#table_number")["0"].innerHTML = ("Table number:  " + " " + (container["0"].innerHTML));
+//  console.log($("#table_number")["0"].innerHTML);
 });
 
-$("button").click(function(){
+$("button").on("click", function(event){
+  users_name = $("#name").val();
+  users_number = $("#number").val();
+  party_size = $("#size").val();
+  container.attr("class", "circles reserved");
+  $(".reserved").off("click");
   $("#form").hide();
 });
 
+$(".circles").on("mouseenter", function() {
+  if ($(this).attr("class") === "circles reserved") {
+    (container["0"].innerHTML) = users_name;
+  }
+});
+
+$(".circles").on("mouseleave", function() {
+  if ($(this).attr("class") === "circles reserved") {
+    (container["0"].innerHTML) = inside_text;
+  }
+});
 //--------------------------------
 });
